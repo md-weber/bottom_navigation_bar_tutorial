227d02d4005ce4286ab54ab5072bcf49e9a1dc27
myracledesign <weber.max.1988@live.de>
2020-02-25 13:49:23 +0100

    added an image

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Image.asset("assets/undraw_reading_book.png"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
